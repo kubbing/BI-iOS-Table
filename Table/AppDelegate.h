16e8913b86415ce15d7668e83bc90cc7808f83e6
Jakub Hladik <kubbing@me.com>
2012-10-17 18:42:28 +0200

    Initial Commit

---
+++ Table/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Table
+//
+//  Created by Jakub Hladík on 17.10.12.
+//  Copyright (c) 2012 Jakub Hladík. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Table/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Table
+//
+//  Created by Jakub Hladík on 17.10.12.
+//  Copyright (c) 2012 Jakub Hladík. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
